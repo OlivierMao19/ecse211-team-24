dbff25fb67b057a62ac258b06b873f2739a17ba8
Olivier Mao <oliviermao2005@gmail.com>
2026-01-29 13:32:51 -0500

    Fixed script

--- lab2-starter-code-team-24/project/collect_color_sensor_data.py
+++ lab2-starter-code-team-24/project/collect_color_sensor_data.py
@@ -8,8 +8,7 @@ It must be run on the robot.
 # Add your imports here, if any
 from utils.brick import EV3ColorSensor, wait_ready_sensors, TouchSensor
 
-
-COLOR_SENSOR_DATA_FILE = "../data_analysis/color_sensor.csv"
+COLOR_SENSOR_DATA_FILE = "../data_analysis/color_sensor-red.csv"
 
 # complete this based on your hardware setup
 COLOR_SENSOR = EV3ColorSensor(2)
@@ -21,18 +20,20 @@ wait_ready_sensors(True) # Input True to see what the robot is trying to initial
 def collect_color_sensor_data():
     "Collect color sensor data."
     try:
-        with open(COLOR_SENSOR_DATA_FILE, "w") as output_file:
-            print("Press touch sensor to record one RGB sample.")
-            while True:
-                if TOUCH_SENSOR.is_pressed():
-                    rgb = COLOR_SENSOR.get_rgb()
-                    if rgb is not None:
-                        r, g, b = rgb
-                        print(f"{r},{g},{b}")
-                        output_file.write(f"{r},{g},{b}\n")
-                    # wait for release to avoid multiple samples per press
-                    while TOUCH_SENSOR.is_pressed():
-                        pass
+        print("Press touch sensor to record one RGB sample.")
+        while True:
+            if TOUCH_SENSOR.is_pressed():
+                rgb = COLOR_SENSOR.get_rgb()
+                if rgb is not None:
+                    r, g, b = rgb
+                    print(f"{r},{g},{b}")
+                    
+                    with open(COLOR_SENSOR_DATA_FILE, "a") as f:
+                        f.write(f"{r},{g},{b}\n")
+                # wait for release to avoid multiple samples per press
+                while TOUCH_SENSOR.is_pressed():
+                    pass
+                
     except BaseException:
         exit()
 
